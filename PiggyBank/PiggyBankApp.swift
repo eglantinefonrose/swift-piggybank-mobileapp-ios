43faee80315ea67053ff505aee4c81875e8aae96
Eglantine Fonrose <eglantine.fonrose@gmail.com>
2023-07-19 21:13:16 +0200

    Initial Commit

---
+++ PiggyBank/PiggyBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiggyBankApp.swift
+//  PiggyBank
+//
+//  Created by Eglantine Fonrose on 19/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct PiggyBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
